2252359f070faf11ad13a832cdeb10e2364a7bba
pengwenish <vitalii_logvinov@mail.ru>
2020-01-24 17:49:11 +0700

    empty game

--- Game.py
+++ Game.py
@@ -22,25 +22,23 @@ def terminate():
 
 def start_screen():
     fon = pygame.transform.scale(load_image('startscreen.jpg'), (1920, 1080))
-    play_sprites = pygame.sprite.Group()
-    exit_sprites = pygame.sprite.Group()
-    cur_sprites = pygame.sprite.Group()
+    start_sprites = pygame.sprite.Group()
     playbut = pygame.sprite.Sprite()
     playbut.image = load_image('play.png')
     playbut.rect = playbut.image.get_rect()
     playbut.rect.x = 650
     playbut.rect.y = 480
-    play_sprites.add(playbut)
+    start_sprites.add(playbut)
     exitbut = pygame.sprite.Sprite()
     exitbut.image = load_image('exit.png')
     exitbut.rect = exitbut.image.get_rect()
     exitbut.rect.x = 650
     exitbut.rect.y = 700
-    exit_sprites.add(exitbut)
+    start_sprites.add(exitbut)
     cur = pygame.sprite.Sprite()
     cur.image = load_image("cross.png")
     cur.rect = cur.image.get_rect()
-    cur_sprites.add(cur)
+    start_sprites.add(cur)
     while True:
         screen.blit(fon, (0, 0))
         for event in pygame.event.get():
@@ -49,12 +47,33 @@ def start_screen():
             if event.type == pygame.MOUSEMOTION:
                 cur.rect.center = event.pos
             if event.type == pygame.MOUSEBUTTONDOWN and playbut.rect.collidepoint(event.pos):
-                pass
+                game(event.pos)
             if event.type == pygame.MOUSEBUTTONDOWN and exitbut.rect.collidepoint(event.pos):
                 terminate()
-        play_sprites.draw(screen)
-        exit_sprites.draw(screen)
-        cur_sprites.draw(screen)
+        start_sprites.draw(screen)
+        clock.tick(FPS)
+        pygame.display.flip()
+
+
+def game(pos):
+    fon = pygame.transform.scale(load_image('bg.png'), (1920, 1080))
+    game_sprites = pygame.sprite.Group()
+    cur = pygame.sprite.Sprite()
+    cur.image = load_image("cross.png")
+    cur.rect = cur.image.get_rect()
+    cur.rect.x = pos[0] - 75
+    cur.rect.y = pos[1] - 75
+    game_sprites.add(cur)
+    while True:
+        screen.blit(fon, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEMOTION:
+                cur.rect.center = event.pos
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                start_screen()
+        game_sprites.draw(screen)
         clock.tick(FPS)
         pygame.display.flip()
 
