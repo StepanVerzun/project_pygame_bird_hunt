ee39a6dfad9c178866f0f7e247467351bbd02c1e
StepanVerzun <stepan.verzun@mail.ru>
2020-01-25 15:09:51 +0700

    Окно выбора кол-ва птиц + "press escape to exit"

--- Game.py
+++ Game.py
@@ -55,7 +55,7 @@ def start_screen():
             if event.type == pygame.MOUSEMOTION:
                 cur.rect.center = event.pos
             if event.type == pygame.MOUSEBUTTONDOWN and playbut.rect.collidepoint(event.pos):
-                game(event.pos)
+                mode_screen()
             if event.type == pygame.MOUSEBUTTONDOWN and exitbut.rect.collidepoint(event.pos):
                 terminate()
         start_sprites.draw(screen)
@@ -63,7 +63,50 @@ def start_screen():
         pygame.display.flip()
 
 
-def game(pos):
+def mode_screen():
+    fon = pygame.transform.scale(load_image('modescreen.jpg'), (1920, 1080))
+    choose_sprites = pygame.sprite.Group()
+    one_but = pygame.sprite.Sprite()
+    one_but.image = load_image('1bird.png')
+    one_but.rect = one_but.image.get_rect()
+    one_but.rect.x = 650
+    one_but.rect.y = 495
+    choose_sprites.add(one_but)
+    two_but = pygame.sprite.Sprite()
+    two_but.image = load_image('2birds.png')
+    two_but.rect = two_but.image.get_rect()
+    two_but.rect.x = 650
+    two_but.rect.y = 640
+    choose_sprites.add(two_but)
+    three_but = pygame.sprite.Sprite()
+    three_but.image = load_image('3birds.png')
+    three_but.rect = three_but.image.get_rect()
+    three_but.rect.x = 650
+    three_but.rect.y = 785
+    choose_sprites.add(three_but)
+    cur = pygame.sprite.Sprite()
+    cur.image = load_image("cross.png")
+    cur.rect = cur.image.get_rect()
+    choose_sprites.add(cur)
+    while True:
+        screen.blit(fon, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.MOUSEMOTION:
+                cur.rect.center = event.pos
+            if event.type == pygame.MOUSEBUTTONDOWN and one_but.rect.collidepoint(event.pos):
+                game(event.pos, 1)
+            if event.type == pygame.MOUSEBUTTONDOWN and two_but.rect.collidepoint(event.pos):
+                game(event.pos, 2)
+            if event.type == pygame.MOUSEBUTTONDOWN and three_but.rect.collidepoint(event.pos):
+                game(event.pos, 3)
+        choose_sprites.draw(screen)
+        clock.tick(FPS)
+        pygame.display.flip()
+
+
+def game(pos, kolvo):
     class AnimatedSprite(pygame.sprite.Sprite):
         def __init__(self, sheet, columns, rows, x, y):
             super().__init__(bird_sprites)
@@ -112,21 +155,34 @@ def game(pos):
     score.rect.x = 50
     score.rect.y = 900
     game_sprites.add(score)
+    escape = pygame.sprite.Sprite()
+    escape.image = load_image('ESC.png')
+    escape.rect = escape.image.get_rect()
+    escape.rect.x = 0
+    escape.rect.y = 0
+    game_sprites.add(escape)
     bullets = pygame.sprite.Sprite()
     bullets.image = load_image("bullets.png")
     bullets.rect = cur.image.get_rect()
     bullets.rect.x = 50
     bullets.rect.y = 1000
     game_sprites.add(bullets)
-    bird = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -100, 360)
-    bird_sprites.add(bird)
+    if kolvo == 1:
+        bird = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -100, 360)
+        bird_sprites.add(bird)
+    elif kolvo == 2:
+        bird1 = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -100, 360)
+        bird_sprites.add(bird1)
+        bird2 = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -500, 360)
+        bird_sprites.add(bird2)
+    elif kolvo == 3:
+        bird1 = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -100, 360)
+        bird_sprites.add(bird1)
+        bird2 = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -500, 360)
+        bird_sprites.add(bird2)
+        bird3 = AnimatedSprite(load_image("bird-sprite.png"), 5, 3, -900, 360)
+        bird_sprites.add(bird3)
     game_sprites.add(cur)
-    vyhod = pygame.sprite.Sprite()
-    vyhod.image = load_image("ESC.png")
-    vyhod.rect = cur.image.get_rect()
-    vyhod.rect.x = 10
-    vyhod.rect.y = 10
-    game_sprites.add(vyhod)
     counter, text = 0, 'charged'.rjust(3)
     pygame.time.set_timer(pygame.USEREVENT, 1000)
     font = pygame.font.SysFont('Consolas', 100)
@@ -139,31 +195,110 @@ def game(pos):
                 cur.rect.center = event.pos
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 start_screen()
-            if event.type == pygame.MOUSEBUTTONDOWN and bird.rect.collidepoint(event.pos):
-                if counter == 0:
-                    points += shots * 10
-                    shots = 3
-                    death.play()
-                    shot.set_volume(0.5)
-                    counter = 3
-                    shot.play()
-                    bird.rect.x = -800
-                    bird.vx += 0.25
-                    create_particles(pygame.mouse.get_pos())
-                else:
-                    dontshot.play()
-            if event.type == pygame.MOUSEBUTTONDOWN and not bird.rect.collidepoint(event.pos):
-                if counter == 0:
-                    shots -= 1
-                    if shots == 0:
+            if kolvo == 1:
+                if event.type == pygame.MOUSEBUTTONDOWN and bird.rect.collidepoint(event.pos):
+                    if counter == 0:
+                        points += shots * 10
+                        shots = 3
+                        death.play()
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
                         bird.rect.x = -800
                         bird.vx += 0.25
+                        create_particles(pygame.mouse.get_pos())
+                    else:
+                        dontshot.play()
+                if event.type == pygame.MOUSEBUTTONDOWN and not bird.rect.collidepoint(event.pos):
+                    if counter == 0:
+                        shots -= 1
+                        if shots == 0:
+                            bird.rect.x = -800
+                            bird.vx += 0.25
+                            shots = 3
+                            points = 0
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
+                    else:
+                        dontshot.play()
+            elif kolvo == 2:
+                if event.type == pygame.MOUSEBUTTONDOWN and (bird1.rect.collidepoint(event.pos)
+                                                             or bird2.rect.collidepoint(event.pos)):
+                    if counter == 0:
+                        points += shots * 10
                         shots = 3
-                    shot.set_volume(0.5)
-                    counter = 3
-                    shot.play()
-                else:
-                    dontshot.play()
+                        death.play()
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
+                        if bird1.rect.collidepoint(event.pos):
+                            bird1.rect.x = -800
+                            bird1.vx += 0.25
+                        elif bird2.rect.collidepoint(event.pos):
+                            bird2.rect.x = -800
+                            bird2.vx += 0.25
+                        create_particles(pygame.mouse.get_pos())
+                    else:
+                        dontshot.play()
+                if event.type == pygame.MOUSEBUTTONDOWN and not (bird1.rect.collidepoint(event.pos)
+                                                                 or bird2.rect.collidepoint(event.pos)):
+                    if counter == 0:
+                        shots -= 1
+                        if shots == 0:
+                            bird1.rect.x = -800
+                            bird2.rect.x = -1200
+                            bird1.vx += 0.25
+                            bird2.vx += 0.25
+                            shots = 3
+                            points = 0
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
+                    else:
+                        dontshot.play()
+            elif kolvo == 3:
+                if event.type == pygame.MOUSEBUTTONDOWN and (bird1.rect.collidepoint(event.pos)
+                                                             or bird2.rect.collidepoint(event.pos)
+                                                             or bird3.rect.collidepoint(event.pos)):
+                    if counter == 0:
+                        points += shots * 10
+                        shots = 3
+                        death.play()
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
+                        if bird1.rect.collidepoint(event.pos):
+                            bird1.rect.x = -800
+                            bird1.vx += 0.25
+                        elif bird2.rect.collidepoint(event.pos):
+                            bird2.rect.x = -800
+                            bird2.vx += 0.25
+                        elif bird3.rect.collidepoint(event.pos):
+                            bird3.rect.x = -800
+                            bird3.vx += 0.25
+                        create_particles(pygame.mouse.get_pos())
+                    else:
+                        dontshot.play()
+                if event.type == pygame.MOUSEBUTTONDOWN and not (bird1.rect.collidepoint(event.pos)
+                                                                 or bird2.rect.collidepoint(event.pos)
+                                                                 or bird3.rect.collidepoint(event.pos)):
+                    if counter == 0:
+                        shots -= 1
+                        if shots == 0:
+                            bird1.rect.x = -800
+                            bird2.rect.x = -1200
+                            bird3.rect.x = -1600
+                            bird1.vx += 0.25
+                            bird2.vx += 0.25
+                            bird3.vx += 0.25
+                            shots = 3
+                            points = 0
+                        shot.set_volume(0.5)
+                        counter = 3
+                        shot.play()
+                    else:
+                        dontshot.play()
             if event.type == pygame.USEREVENT:
                 if counter != 0:
                     counter -= 1
@@ -178,12 +313,13 @@ def game(pos):
         game_sprites.draw(screen)
         clock.tick(FPS)
         screen.blit(font.render(text, True, (255, 0, 0)), (1000, 900))
-        screen.blit(font.render(str(points), True, (255, 0, 0)), (50 + bullets.image.get_width() + 50, 890))
-        screen.blit(font.render(str(shots), True, (255, 0, 0)), (50 + bullets.image.get_width() + 50, 990))
+        screen.blit(font.render(str(points), True, (255, 0, 0)), (50 + bullets.image.get_width() + 50, 900))
+        screen.blit(font.render(str(shots), True, (255, 0, 0)), (50 + bullets.image.get_width() + 50, 1000))
         pygame.display.flip()
 
 
 class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
     fire = [load_image("feather.png")]
     for scale in (5, 10, 20):
         fire.append(pygame.transform.scale(fire[0], (scale, scale)))
